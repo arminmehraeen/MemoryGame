19739500680507313afb1787caf126c38492f6a9
Armin Mehraeen <armin.mehraeen@gmail.com>
2022-11-13 21:41:38 +0330

    add app file to project

---
+++ lib/app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:memory_game/screen/p_menu.dart';
+
+import 'bloc/game_bloc.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+  @override
+
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => GameBloc() ,
+      child: MaterialApp(
+        title: 'Memory Game',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            primaryColor: const Color(0xFF1573AA),
+            appBarTheme: const AppBarTheme(
+              color: Color(0xFF142550) ,
+            )
+        ),
+        home: const MenuScreen(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:memory_game/bloc/game_bloc.dart';
-import 'package:memory_game/screen/p_menu.dart';
-import 'package:memory_game/screen/p_result.dart';
+import 'package:memory_game/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  @override
 
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => GameBloc() ,
-      child: MaterialApp(
-        title: 'Memory Game',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-            primaryColor: const Color(0xFF1573AA),
-            appBarTheme: const AppBarTheme(
-              color: Color(0xFF142550) ,
-            )
-        ),
-        home: const MenuScreen(),
-      ),
-    );
-  }
-}
